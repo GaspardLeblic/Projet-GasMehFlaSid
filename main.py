ebd43ce9e37e605fec9e7423987438e321e397bc
Sidney <130940539+Rupixe@users.noreply.github.com>
2025-01-09 09:41:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ pygame.display.set_caption("The maze")
 screen = pygame.display.set_mode ((1200,700))
 
 
-background = pygame.image.load("IMAGE BG ACCUEIL.jpg")
+background = pygame.image.load("Image/IMAGE BG ACCUEIL.jpg")
 #import charger notre bouton pour lancer la partie
-play_button = pygame.image.load('bouton play.PNG')
-play_button = pygame.transform.scale(play_button,(500,500))
+fond_ecran_menu = pygame.image.load('Image/bouton play.PNG')
+play_button_menu = pygame.transform.scale(fond_ecran_menu,(500,500))
 
 #perso = pygame.image.load("").convert_alpha()
 #position_perso = perso.get_rect()
@@ -22,7 +22,7 @@ running = True
 while running:
 
     screen.blit(background, (0, 0))
-    screen.blit(play_button,(0,0))
+    screen.blit(fond_ecran_menu,(0,0))
     pygame.display.flip()
 
     for event in pygame.event.get():
@@ -31,4 +31,3 @@ while running:
             running = False
             print ("Fermeture du jeu")
 pygame.quit()
-
